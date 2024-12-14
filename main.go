d445a1715240f2af026d8098d572a59d2ee1b52e
tialiaoLeo <LTP1084879501@GMAIL.COM>
2024-12-14 00:48:08 -0500

    Publish and consume message queue for each node with termination mech

--- main.go
+++ main.go
@@ -6,16 +6,21 @@ import (
 	"time"
 )
 
-// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func (n *Node) k_core_exe(graph map[string][]Edge, nodes map[string]*Node) {
 	// Example logic for k_core, modify as needed
 	n.k_core(graph, nodes)
 	fmt.Printf("Node: %v CoreNum: %v \n", n.NodeID, n.CoreNum)
 }
+
+var done = false
+
+type Global struct {
+	ms_id int
+}
+
 func main() {
 	filePath := "graph.txt"
-
+	global := &Global{0}
 	// Call the ReadGraphFile function
 	graph, err := ReadGraphFile(filePath)
 	if err != nil {
@@ -26,26 +31,56 @@ func main() {
 	nodes := make(map[string]*Node)
 	// Print the graph
 	for key, value := range graph {
-		nodes[key] = &Node{key, len(value)}
+		nodes[key] = &Node{key, len(value), []Message{}}
 	}
 
 	// WaitGroup to wait for both Go routines to finish
 	var wg sync.WaitGroup
-
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		terminate(nodes, global)
+	}()
 	for _, n := range nodes {
 		wg.Add(1)
-		go func(n *Node) {
+		go func(n *Node, global *Global) {
 			defer wg.Done()
 			n.k_core(graph, nodes)
 			fmt.Printf("Node: %v CoreNum: %v \n", n.NodeID, n.CoreNum)
-			if n.NodeID == "g" {
-				time.Sleep(5 * time.Second)
-				n.k_core(graph, nodes)
-				fmt.Printf("After 5 sec Node: %v CoreNum: %v \n", n.NodeID, n.CoreNum)
+			n.publish(nodes, global)
+			for !done {
+				if n.consume() {
+					pre := n.CoreNum
+					n.k_core(graph, nodes)
+					fmt.Printf("Node: %v CoreNum: %v \n", n.NodeID, n.CoreNum)
+					if pre != n.CoreNum {
+						n.publish(nodes, global)
+					}
+				}
 			}
-		}(n)
+		}(n, global)
 	}
 
 	// Wait for both goroutines to complete
 	wg.Wait()
+}
+
+func terminate(nodes map[string]*Node, global *Global) {
+	for {
+		time.Sleep(500 * time.Millisecond)
+		// Check if all nodes have empty Msg_queue
+		allIdle := true
+		for _, node := range nodes {
+
+			if len(node.Msg_queue) > 0 {
+				allIdle = false
+				break
+			}
+		}
+		if allIdle && global.ms_id > 0 {
+			fmt.Println("Termination condition met. Shutting down...")
+			done = true // Notify all Goroutines to terminate
+			return
+		}
+	}
 }
--- node.go
+++ node.go
@@ -1,14 +1,13 @@
 // node.go
 package main
 
+// global variable for message id
+
 // Node represents a node in a graph
 type Node struct {
-	NodeID  string // Unique identifier for the node
-	CoreNum int    // Core number of the node
-}
-
-func (n *Node) IncrementCoreNum() {
-	n.CoreNum++
+	NodeID    string // Unique identifier for the node
+	CoreNum   int    // Core number of the node
+	Msg_queue []Message
 }
 
 func (n *Node) k_core(graph map[string][]Edge, nodes map[string]*Node) {
@@ -27,5 +26,17 @@ func (n *Node) num_neighbors(CoreNum int, graph map[string][]Edge, nodes map[str
 
 	}
 	return res
+}
 
+func (n *Node) publish(nodes map[string]*Node, global *Global) {
+	for _, target := range nodes {
+		n.Msg_queue = append(n.Msg_queue, Message{global.ms_id, n, target})
+		global.ms_id = global.ms_id + 1
+	}
+}
+
+func (n *Node) consume() bool {
+	pub_again := len(n.Msg_queue) > 0
+	n.Msg_queue = nil
+	return pub_again
 }
